a9ab8b44bbddb82198ce64790253c8a45d3bddbe
ArjunPC1121 <chandraarjun2107@gmail.com>
2025-08-11 15:40:32 +0530

    Add uploadedFile model for spreadsheet uploads

--- spreadsheet_manager/core/models.py
+++ spreadsheet_manager/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UploadedFile(models.Model):
+    file = models.FileField(upload_to='spreadsheets/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Spreadsheet {self.id} uploaded at {self.uploaded_at}"
