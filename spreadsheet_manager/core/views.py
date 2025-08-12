b6674ae2ec049a8b58f5c7d9f7c66db73d1d6b11
ArjunPC1121 <chandraarjun2107@gmail.com>
2025-08-12 15:57:48 +0530

    Add file upload form, view, template, URL routing, crispy-forms config, and MEDIA settings

--- spreadsheet_manager/core/views.py
+++ spreadsheet_manager/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
 
-# Create your views here.
+def home(request):
+    
+    if request.method == 'POST':
+        form=UploadFileForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    else:
+        form = UploadFileForm()
+        
+    return render(request,'core/home.html',{'form':form})
+
+
--- spreadsheet_manager/spreadsheet_manager/settings.py
+++ spreadsheet_manager/spreadsheet_manager/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -123,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files (user uploads)
+MEDIA_URL = '/media/'  
+MEDIA_ROOT = BASE_DIR / 'media'
+
