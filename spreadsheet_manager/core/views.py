eaf7314beef37df79e832d9320d319aef6f986e4
ArjunPC1121 <chandraarjun2107@gmail.com>
2025-08-14 22:25:27 +0530

    Add pandas spreadsheet preview after file upload

--- spreadsheet_manager/core/views.py
+++ spreadsheet_manager/core/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, redirect
 from .forms import UploadFileForm
+import pandas as pd
 
 def home(request):
+    preview_html = None
     
     if request.method == 'POST':
         form=UploadFileForm(request.POST, request.FILES)
         
         if form.is_valid():
-            form.save()
-            return redirect('home')
+            uploaded_file=form.save()
+            
+            file_path=uploaded_file.file.path
+            
+            try:
+                if file_path.endswith('.csv'):
+                    df = pd.read_csv(file_path)
+                else:
+                    df=pd.read_excel(file_path)
+                
+                preview_html=df.head().to_html(classes='table table-striped', index=False)
+            
+            except Exception as e:
+                preview_html=f"<p style='color:red;'>Could not preview file: {str(e)}</p>"
+        
+        return render(request,'core/home.html',{'form':form,'preview_html':preview_html})
     
     else:
         form = UploadFileForm()
